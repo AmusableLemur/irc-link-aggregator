4fe9f7a6ba0100ded3b47cf7fa5997914bda2cb1
Rasmus Larsson <meister@rasmuslarsson.se>
2013-08-26 20:07:49 +0200

    Added security

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__."/../vendor/autoload.php";
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 $app = new Silex\Application(array(
     "debug" => true
@@ -20,9 +21,33 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     )
 ));
 
+$app->register(new Silex\Provider\SecurityServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     "twig.path" => __DIR__."/../views",
 ));
+
+$app["password"] = function() {
+    $encoder = new MessageDigestPasswordEncoder();
+    $password = "CHANGE_ME";
+    $password = md5($password.date("Y-m-d"));
+    $password = substr($password, 0, 12);
+
+    return $encoder->encodePassword($password, "");
+};
+
+$app["security.firewalls"] = array(
+    "unsecured" => array(
+        "pattern" => "^/submit",
+    ),
+    "secured" => array(
+        "pattern" => "^/",
+        "http" => true,
+        "users" => array(
+            "user" => array("ROLE_USER", $app["password"]),
+        ),
+    ),
+);
 
 $app->get("/", function() use($app) {
     $query = $app["db"]->prepare("SELECT * FROM links ORDER BY id DESC");
@@ -34,8 +59,15 @@ $app->get("/", function() use($app) {
     ));
 });
 
-$app->post("/", function(Request $request) use($app) {
-    if ($request->get("key") == "secret") {
+$app->get("/login", function() use($app) {
+    return $app["twig"]->render("login.html.twig", array(
+        "error" => $app["security.last_error"]($request),
+        "last_username" => $app["session"]->get("_security.last_username")
+    ));
+});
+
+$app->post("/submit", function(Request $request) use($app) {
+    if ($request->get("key") == "CHANGE_ME") {
         $query = $app["db"]->prepare(
             "INSERT INTO links (
                 url,
@@ -50,7 +82,7 @@ $app->post("/", function(Request $request) use($app) {
 
         $query->bindValue("url", $request->get("url"));
         $query->bindValue("nick", $request->get("nick"));
-        $query->bindValue("time", $request->get("time"));
+        $query->bindValue("time", time());
         $query->execute();
     }
 
